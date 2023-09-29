3d6e466247f3829ab3c430c024fafb9ee2adea8e
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-09-29 14:09:04 +1000

    Completes parts 3 & 4 for files.py section of Practical 3.

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,8 +3,25 @@
 with open("name.txt", "w") as out_file:
     user_name = input("Enter your name: ")
     print(user_name, file=out_file)
+print()
 
 # Part 2
 with open("name.txt", "r") as in_file:
     user_name = in_file.readline().strip()
     print(f"Your name is: {user_name}")
+print()
+
+# Part 3
+with open("numbers.txt", "r") as in_file:
+    total = 0
+    for i in range(2):
+        total += int(in_file.readline().strip())
+    print(f"Total is: {total}")
+print()
+
+# Part 4
+with open("numbers.txt", "r") as in_file:
+    total = 0
+    for line in in_file:
+        total += int(line.strip())
+    print(f"Total is: {total}")
