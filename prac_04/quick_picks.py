0b38e25c5fdad325883c9e5a0c541bc5659864c0
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-10-03 18:35:53 +1000

    Refactor to main() structure

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,13 +1,32 @@
 """CP1404 Practical 04: Generates a number of quick picks based on a user input"""
 import random
 
-number_of_quick_picks = int(input("How many quick picks? "))
-for i in range(number_of_quick_picks):
+LOW_NUMBER = 1
+HIGH_NUMBER = 45
+NUMBERS_PER_QUICK_PICK = 6
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        quick_pick = generate_quick_pick(NUMBERS_PER_QUICK_PICK)
+        print_quick_pick(quick_pick)
+
+
+def generate_quick_pick(numbers_per_quick_pick):
     quick_pick = []
-    for j in range(6):
-        number = random.randint(1, 45)
+    for j in range(numbers_per_quick_pick):
+        number = random.randint(LOW_NUMBER, HIGH_NUMBER)
         while number in quick_pick:
             if number in quick_pick:
-                number = random.randint(1, 45)
+                number = random.randint(LOW_NUMBER, HIGH_NUMBER)
         quick_pick.append(number)
-    print(" ".join([f"{number:2}" for number in quick_pick]))
+    return quick_pick
+
+
+def print_quick_pick(quick_pick):
+    max_length = len(str(HIGH_NUMBER))
+    print(" ".join([f"{number:{max_length}}" for number in quick_pick]))
+
+
+main()
