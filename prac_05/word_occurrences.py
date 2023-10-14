1f80f28f944880aa95d44fdb0015caded88b20f7
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-10-14 12:33:47 +1000

    Prac05: Add formatted printing and sorting to word_occurrences.py exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 CP1404 Practical 05: Word Occurrences
 Estimated Time to Complete: 20 minutes
-Actual Time to Complete:
+Actual Time to Complete: 16 minutes
 """
 
 string = input("Text: ")
@@ -12,4 +12,7 @@ for word in words:
         word_to_count[word] += 1
     except KeyError:
         word_to_count[word] = 1
-print(word_to_count)
+max_word_length = max((len(word) for word in words))
+word_to_count = dict(sorted(word_to_count.items()))
+for word in word_to_count:
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
