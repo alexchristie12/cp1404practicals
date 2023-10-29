ff864cad3dcdb89b3b657c05da70a64bf6aa9a23
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-10-29 13:36:22 +1000

    Prac06: Fix formatting

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,6 +2,7 @@
 
 from prac_06.guitar import Guitar
 
+
 def main():
     """Program that stores a list of Guitar and their details."""
     guitars = []
@@ -27,7 +28,7 @@ def main():
             vintage_string = " (vintage)"
         else:
             vintage_string = ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year:>4}), worth ${guitar.cost:12,.2f}{vintage_string}")
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year:>4}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
 if __name__ == "__main__":
