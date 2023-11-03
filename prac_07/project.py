d27430a19a81fc0377ddbed4ebb2b323399f0b7e
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-11-03 15:38:21 +1000

    Prac07: Add docstrings for Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,12 @@
 """CP1404 Practical 07: Project Mangement Task"""
 import datetime
 
+
 class Project:
     """A Project object."""
 
     def __init__(self, name="", start_date="01/01/2000", priority=1, cost_estimate=0.00, completion_percentage=0):
+        """Instantiate a Project object."""
         self.name = name
         # self.start_date = start_date
         try:
@@ -18,6 +20,7 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
+        """String formatting for use in print statements, etc."""
         date_string = self.start_date.strftime("%d/%m/%Y")
         return f"{self.name}, start: {date_string}, priority: {self.priority}, estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}%"
 
@@ -25,5 +28,6 @@ class Project:
         return self.priority < other.priority
 
     def storage_format(self):
+        """Returns a string in correct format for storage in .txt file."""
         date_string = self.start_date.strftime("%d/%m/%Y")
         return f"{self.name}\t{date_string}\t{self.priority}\t{self.cost_estimate:.1f}\t{self.completion_percentage}\n"
