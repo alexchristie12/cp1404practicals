6dfb914151f9a60680e49e18ce0c8383d6778aeb
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-11-11 16:18:42 +1000

    Prac08: Introduce constant for miles to km conversion

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -9,6 +9,8 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.properties import StringProperty
 
+MILES_TO_KM = 1.60934
+
 
 class MilesToKmApp(App):
     """Insert docstring"""
@@ -23,7 +25,7 @@ class MilesToKmApp(App):
 
     def handle_new_value(self):
         miles = self.get_valid_input()
-        kilometres = miles * 1.60934
+        kilometres = miles * MILES_TO_KM
         self.root.ids.output_label.text = f"{kilometres}"
 
     def get_valid_input(self):
