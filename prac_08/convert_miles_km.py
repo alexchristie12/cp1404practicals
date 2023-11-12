8c7fb8d74726abb366d87e31a287f3a73167ca9e
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-11-12 22:04:43 +1000

    Prac08: Add relevant docstrings

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,10 +13,11 @@ MILES_TO_KM = 1.60934
 
 
 class MilesToKmApp(App):
-    """Insert docstring"""
+    """App to convert miles to kilometres."""
     miles_input = StringProperty()
 
     def build(self):
+        """Build the app."""
         Window.size = (500, 300)
         self.title = "Convert Miles to km"
         self.root = Builder.load_file("convert_miles_km.kv")
@@ -24,11 +25,13 @@ class MilesToKmApp(App):
         return self.root
 
     def handle_new_value(self):
+        """Handles new value and converts it to miles."""
         miles = self.get_valid_input()
         kilometres = miles * MILES_TO_KM
         self.root.ids.output_label.text = f"{kilometres}"
 
     def get_valid_input(self):
+        """Get a valid input from a text field."""
         try:
             new_value = float(self.root.ids.miles_text_input.text)
             return new_value
@@ -36,6 +39,7 @@ class MilesToKmApp(App):
             return 0.0
 
     def handle_increment(self, increment):
+        """Handles a increment to the current value in the text field and increments it by a particular amount."""
         value = self.get_valid_input() + increment
         self.root.ids.miles_text_input.text = f"{value}"
         self.handle_new_value()
