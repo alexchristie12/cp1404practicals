6ebc666d21f6f07ed55fb3c93865f4967d548604
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-11-12 22:06:57 +1000

    Prac08: Add relevant docstrings

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,15 +8,19 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Program that greets the user based on a name inputted by the user."""
     def build(self):
+        """Builds the box layout demo app."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Greets the user."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clears the output label"""
         self.root.ids.output_label.text = ""
 
 
