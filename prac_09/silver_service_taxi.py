d8f31ab6786bbbbe04a8e9008d0f3899c72fd197
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-11-16 22:03:41 +1000

    Prac09: Add str and constructor docstrings to silver service taxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -12,10 +12,12 @@ class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness: float):
+        """Create a Silver Service Taxi class, based on parent Taxi class."""
         super().__init__(name, fuel)
         self.price_per_km = Taxi.price_per_km * fanciness
 
     def __str__(self):
+        """Return a string like the Taxi parent class but with the flag fall price added."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall}."
 
     def get_fare(self):
